2a94c777fdfead8f2c7e4276bbdeaff74835b40a
zuniraptor8 <angelzh98@gmail.com>
2023-07-13 18:57:19 -0600

    v11

--- app.js
+++ app.js
@@ -17,9 +17,8 @@ function initMap() {
       "mapDisplayControl": true,
       "ctaControl": true
     }
-    
-    
   };
+
   const componentForm = [
     'location',
     'locality',
@@ -27,10 +26,9 @@ function initMap() {
     'country',
     'postal_code',
   ];
-  
-  
-  
+
   const getFormInputElement = (component) => document.getElementById(component + '-input');
+
   const map = new google.maps.Map(document.getElementById("7e96eb91e6c45c79"), {
     zoom: CONFIGURATION.mapOptions.zoom,
     mapId: '7e96eb91e6c45c79',
@@ -66,9 +64,8 @@ function initMap() {
       scale: 12
     },
     zIndex: -1
-    
   });
-  // Add a click event listener to the map for updating the marker's position
+
   google.maps.event.addListener(map, 'click', function(event) {
     marker.setPosition(event.latLng);
     marker.setVisible(true);
@@ -85,21 +82,18 @@ function initMap() {
         }
       }
     });
-    
   });
-  
 
-map.addListener("center_changed", () => {
-  // 3 seconds after the center of the map has changed, pan back to the
-  // marker.
-  window.setTimeout(() => {
-    map.panTo(marker.getPosition());
-  }, 3000);
-});
-marker.addListener("click", () => {
-  map.setZoom(20);
-  map.setCenter(marker.getPosition());
-});
+  map.addListener("center_changed", () => {
+    window.setTimeout(() => {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+
+  marker.addListener("click", () => {
+    map.setZoom(20);
+    map.setCenter(marker.getPosition());
+  });
 
   autocomplete.addListener('place_changed', function () {
     marker.setVisible(true);
@@ -112,7 +106,6 @@ marker.addListener("click", () => {
     fillInAddress(place);
   });
 
-  
   marker.addListener('dragend', function () {
     const newMarkerPosition = marker.getPosition();
     const newMarkerLatitude = newMarkerPosition.lat();
@@ -188,6 +181,9 @@ marker.addListener("click", () => {
         getFormInputElement(component).value = getAddressComp(component);
       }
     }
+
+    // Here is where we call the function to transfer values
+    transferValuesToFluentForms();
   }
 
   function renderAddress(place) {
@@ -211,50 +207,44 @@ marker.addListener("click", () => {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-
           map.setCenter(pos);
           marker.setPosition(pos);
           marker.setVisible(true);
           markerGlow.setPosition(pos);
           markerGlow.setVisible(true);
 
-          const infoWindow = new google.maps.InfoWindow({
-            content: "¿Este es tu inmueble? 😊",
-          });
-          infoWindow.open(map, marker);
-
           fillInAddressFromGeolocation(position.coords.latitude, position.coords.longitude);
         },
         () => {
-          handleLocationError(true, map.getCenter());
+          handleLocationError(true, infoWindow, map.getCenter());
         }
       );
     } else {
-      handleLocationError(false, map.getCenter());
+      handleLocationError(false, infoWindow, map.getCenter());
     }
-    
   });
+  
+  // This is the new function that will transfer the values from the Google Maps fields to the Fluent Forms fields
+  function transferValuesToFluentForms() {
+    // Mapping of Google Maps fields to Fluent Forms fields
+    const fieldMapping = {
+      'location': 'ff_9_address_1_address_line_1_',
+      'locality': 'ff_9_address_1_city_',
+      'administrative_area_level_1': 'ff_9_address_1_state_',
+      'postal_code': 'ff_9_address_1_zip_',
+      'country': 'ff_9_address_1_country_',
+    };
 
-  function fillInAddressFromGeolocation(latitude, longitude) {
-    const geocoder = new google.maps.Geocoder();
-    const latLng = new google.maps.LatLng(latitude, longitude);
+    // For each Google Maps field...
+    Object.keys(fieldMapping).forEach(googleMapsField => {
+      // Get the value from the Google Maps field
+      const googleMapsValue = getFormInputElement(googleMapsField).value;
 
-    geocoder.geocode({'latLng': latLng}, function (results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        if (results[0]) {
-          const place = results[0];
-          renderAddress(place);
-          fillInAddress(place);
-                }
-      }
+      // Transfer the value to the corresponding Fluent Forms field
+      const fluentFormsId = fieldMapping[googleMapsField];
+      document.getElementById(fluentFormsId).value = googleMapsValue;
     });
   }
-
-  // Hide street view option
-  map.setOptions({streetViewControl: false});
 }
 
-
 window.initMap = initMap;
-
-
